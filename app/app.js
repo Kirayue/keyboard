6fc10fbfe587213e944b1f2ea123db05f9d51f02
dirty <darby.chang@gmail.com>
2016-07-09 10:03:22 +0800

    calculateStat()

--- app/app.js
+++ app/app.js
@@ -3,15 +3,29 @@ import './app.sass'
 let app = {
   history: [],
   keyPool: 'abcdefghijklmnopqrstuvwxyz'.split(''),
+  opt: { nKeyPerRound: 3 },
   targetKeys: [],
 }
-let opt = {
-  nKeyPerRound: 3,
+
+function calculateStat(app) { //! move to server
+  let history = app.history
+  let first = history.shift()
+  let stat = {
+    nCorrect: 0,
+    nKey: history.length,
+  }
+  for (let i of history) {
+    if (i.user.id === i.target.id) stat.nCorrect++
+  }
+  stat.accuracy = stat.nCorrect / stat.nKey
+  //! list required statistics here
+  console.log(stat)
+  return stat
 }
 
 $(document).ready(function(){
   function nextRound(color) {
-    for (let i=0; i<opt.nKeyPerRound; i++) {
+    for (let i=0; i<app.opt.nKeyPerRound; i++) {
       let iKey = Math.floor((Math.random() * app.keyPool.length))
       let key = {
         color: color,
@@ -23,12 +37,18 @@ $(document).ready(function(){
     } 
   }
 
+  $('#stop').click(() => {
+    //! send app to server via ajax instead of calling calculateStat()
+    calculateStat(app)
+  })
+
   $('.key').tap(event => {
     let key = app.targetKeys.shift()
     $('#'+key.id).removeClass(key.color).text('') // restore key
     app.keyPool.push(key.id) // push key back to pool
     app.history.push({
       target: key,
+      timestamp: new Date(),
       user: {
         id: event.target.getAttribute('id'),
         x: event.pageX,
