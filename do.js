07f6284afcceb734405a6e4e2e4bff8617855e83
dirty <darby.chang@gmail.com>
2016-07-16 14:02:01 +0800

    rename click to stroke, need test

--- do.js
+++ do.js
@@ -1,78 +1,78 @@
-//! space character
 import csvStringify from 'csv-stringify'
 import fs from 'fs'
-import querystring from 'querystring'
 import moment from 'moment/moment.js'
+import querystring from 'querystring'
 
-let calculateStat = (trial) => { 
-  let history = trial.history
-  let last = history.shift()
+function calculateStat(trial)
+  let strokes = trial.strokes
+  let lastStroke = strokes.shift()
   let stat = {
-    clicks: [],
+    keyCenter: {},
     nCorrect: 0,
-    nKey: history.length,
-    mean: {},
+    nStroke: history.length,
+    strokes: [],
+    strokeCenter: {},
     timestamp: trial.timestamp,
-    //WPM: (history.length - 1) / (history[history.length-1].timestamp-history[0].timestamp)*1000*60/5 // word per minute
-    WPM: history.length / (history[history.length-1].timestamp - last.timestamp) * 1000 * 60, // word per minute
+    // word per minute, ~5 key strokes per word
+    WPM: (strokes.length - 1) / (strokes[strokes.length-1].timestamp - lastStroke.timestamp) * 1000 * 60 / 5,
   }
-  for (let i of trial.keyList) { // loop for initial mean
-    stat.mean[i] = { x: 0, y: 0 , nTap: 0 }
+
+  // stroke center
+  for (let i of trial.keyList) {
+    //! init stat.keyCenter here
+    stat.strokeCenter[i] = { n: 0, x: 0, y: 0 }
   }
-  for (let cur of history) { //loop for add user.x and user.y
-    let { id, x, y}  = cur.user
-    if (cur.user.id === cur.target.id){
-      stat.nCorrect++
-      stat.mean[id].x += x
-      stat.mean[id].y += y
-      stat.mean[id].nTap++
-    }
+  for (let i of strokes) {
+    if (i.user.id !== cur.target.id) continue
+    stat.nCorrect++
+    stat.strokeCenter[cur.user.id].x += cur.user.x
+    stat.strokeCenter[cur.user.id].y += cur.user.y
+    stat.strokeCenter[cur.user.id].n++
   }
-  for (let key of trial.keyList){ //loop for mean x and y
-     stat.mean[key].x  /= stat.mean[key].nTap
-     stat.mean[key].y  /= stat.mean[key].nTap
+  for (let i of trial.keyList) {
+    stat.strokeCenter[i].x /= stat.strokeCenter[i].n
+    stat.strokeCenter[i].y /= stat.strokeCenter[i].n
   }
-  for (let cur of history) { //loop for statistics
-    let click = [], i = 0
-    let { id, x, y} = cur.user
-    let { height, width} = trial.keySize
-    let keyCenter = { y:trial.keyOffset[cur.target.id].top + height/2, x:trial.keyOffset[cur.target.id].left + width/2 }
+
+  let curStroke, i, row
+  function displacement(ref) {
+    stroke[i++] = curStroke.user.x - ref.x // x displacement
+    stroke[i++] = curStroke.user.y - ref.y // y displacement
+    stroke[i++] = Math.abs(row[i-3]) // abs x displacement
+    stroke[i++] = Math.abs(row[i-3]) // abs y displacement
+  }
+  for (curStroke of strokes) {
+    i = 0, id = curStroke.target.id, stroke = []
+    //let {height, width} = trial.keySize
+    //let keyCenter = { y:trial.keyOffset[cur.target.id].top + height/2, x:trial.keyOffset[cur.target.id].left + width/2 }
     let leftBound = keyCenter.x - 0.75*height , rightBound = keyCenter.x + 0.75*height ,
          topBound = keyCenter.y - height ,  downBound = keyCenter.y + height
-    click[i++] = x >= leftBound && x <= rightBound && y >= topBound && y <= downBound ? 'count':'not count'    
-    click[i++] = cur.target.id //target.id
-    click[i++] = cur.user.id //user.id
-    click[i++] = cur.timestamp - last.timestamp // duration
-    click[i++] = x  // x 
-    click[i++] = y  // y 
-    click[i++] = cur.user.x - last.user.x // x displacement
-    click[i++] = cur.user.y - last.user.y // y displacement
-    click[i++] = Math.abs(click[i - 3]) // abs x displacement
-    click[i++] = Math.abs(click[i - 3]) // abs y displacement
-    click[i++] = Math.pow((Math.pow(click[i-3],2)+Math.pow(click[i-3],2)),1/2)
-    click[i++] = click[i - 2]/click[i - 9]
-    click[i++] = x - keyCenter.x //x displacement relates to keyCenter 
-    click[i++] = y - keyCenter.y //y displacement relates to keyCenter
-    click[i++] = Math.abs(click[i - 3])  //abs x displacement relates to keyCenter
-    click[i++] = Math.abs(click[i - 3])  //abs y displacement relates to keyCenter
-    click[i++] = stat.mean[cur.user.id].x // shiftedKey x
-    click[i++] = stat.mean[cur.user.id].y // shiftedKey y
-    click[i++] = x - stat.mean[cur.user.id].x  //x displacement relates to shiftedKey
-    click[i++] = y - stat.mean[cur.user.id].y  //y displacement relates to shiftedKey
-    click[i++] = Math.abs(click[i - 3]) //abs x displacement relates to shiftedKey
-    click[i++] = Math.abs(click[i - 3]) //abs y displacement relates to shiftedKey
+    //! should consider in stroke center calculation?
+    stroke[i++] = x >= leftBound && x <= rightBound && y >= topBound && y <= downBound ? 'count':'not count'    
+    stroke[i++] = curStroke.target.id // target key
+    stroke[i++] = curStroke.user.id // user key
+    stroke[i++] = curStroke.timestamp - lastStroke.timestamp // duration
+    stroke[i++] = curStroke.user.x // x 
+    stroke[i++] = curStroke.user.y // y 
+    displacement(lastStroke.user.x) // displacement related last stroke
+    stroke[i++] = Math.sqrt(stroke[i-3] * stroke[i-3] + stroke[i-2] * stroke[i-2]) // displacement to the last stroke
+    stroke[i++] = stroke[i-2] / stroke[i-9] // speed (displacement / duration)
+    displacement(stat.keyCenter[id]) // displacement related key center
+    stroke[i++] = stat.strokeCenter[id].x // x of stroke center
+    stroke[i++] = stat.strokeCenter[id].y // y of stroke center
+    displacement(stat.strokeCenter[id]) // displacement related stroke center
 
-    stat.clicks.push(click)
-    last = cur
+    stat.strokes.push(row)
+    lastStroke = curStroke
   }
-  stat.clicks.unshift(['Count','Target','User','Duration(ms)','X','Y','displacement X','displacement Y','abs X','abs Y','distance','velocity(px/ms)','X to keyCenter ','Y to keyCenter','abs X to keyCenter','abs Y to keyCenter','shifted X','shifted Y','X to shiftedKey','Y to shiftedKey','abs X to shiftedKey','abs Y to shiftedKey'])
-  stat.clicks.push([""],["WPM"],[stat.WPM])
+  stat.strokes.unshift(['Count','Target','User','Duration(ms)','X','Y','displacement X','displacement Y','abs X','abs Y','distance','velocity(px/ms)','X to keyCenter ','Y to keyCenter','abs X to keyCenter','abs Y to keyCenter','shifted X','shifted Y','X to shiftedKey','Y to shiftedKey','abs X to shiftedKey','abs Y to shiftedKey'])
+  stat.strokes.push([""],["WPM"],[stat.WPM])
   stat.accuracy = stat.nCorrect / stat.nKey
-  //! list required statistics here
   console.log(stat)
   return stat
 }
-let Do = (query, res) => {
+
+function Do(query, res)
   let path = './tp6vu6bp4/', trial
   if ('string' === typeof query)
     trial = JSON.parse(querystring.parse(query).trial)
